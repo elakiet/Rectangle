b5a2918159525cd810c27544a2cdd48d7b5dcb78
elakiet <elaki.kavin@gmail.com>
2023-09-01 14:31:17 +0530

    initial commit done

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle1=new Rectangle(2,3);
+        Rectangle rectangle2=new Rectangle(4,5);
+
+        System.out.println("Area of Rectangle1 :"+rectangle1.RectangleArea());
+        System.out.println("Circumference of Rectangle1 :" +rectangle1.RectangleCircumference());
+        System.out.println("Area of Rectangle2 :" +rectangle2.RectangleArea());
+        System.out.println("Circumference of Rectangle2 :" +rectangle2.RectangleCircumference());
+
+    }
+}
+class Rectangle{
+    double Length,Breadth;
+    public Rectangle(double Length,double Breadth){
+        this.Length=Length;
+        this.Breadth=Breadth;
+    }
+    double RectangleArea(){
+        return Length*Breadth;
+    }
+
+    double RectangleCircumference(){
+        return 2*(Length+Breadth);
+    }
+}
